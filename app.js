c5213ac56c0e20c51f06c7a2a68ba1e6212bb17f
StevenLian <kingliantop@gmail.com>
2016-12-07 19:15:16 +0800

    validate

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-var wechat = require('wechat');
-var config = {
-  token: 'mytesttokendemo',
-  appid: 'wx00f1e1d61568aa50',
-  encodingAESKey: 'j8iT7Bclw5takfkhxD5Dpz02AVsX7w2GY8bXumDkGGf'
-};
+var http = require("http");
+var url = require("url");
+var crypto = require("crypto");
 
-app.use(express.query());
-app.use('/wechat', wechat(config, function (req, res, next) {
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    // 回复高富帅(图文回复)
-    res.reply([
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ]);
+function sha1(str){
+  var md5sum = crypto.createHash("sha1");
+  md5sum.update(str);
+  str = md5sum.digest("hex");
+  return str;
+}
+
+function validateToken(req,res){
+  var query = url.parse(req.url,true).query;
+  //console.log("*** URL:" + req.url);
+  //console.log(query);
+  var signature = query.signature;
+  var echostr = query.echostr;
+  var timestamp = query['timestamp'];
+  var nonce = query.nonce;
+  var oriArray = new Array();
+  oriArray[0] = nonce;
+  oriArray[1] = timestamp;
+  oriArray[2] = "mytesttokendemo";//这里是你在微信开发者中心页面里填的token，而不是****
+  oriArray.sort();
+  var original = oriArray.join('');
+  console.log("Original str : " + original);
+  console.log("Signature : " + signature );
+  var scyptoString = sha1(original);
+  if(signature == scyptoString){
+    res.end(echostr);
+    console.log("Confirm and send echo back");
+  }else {
+    res.end("false");
+    console.log("Failed!");
   }
-}));
+}
+
+
+var webSvr = http.createServer(validateToken);
+webSvr.listen(process.env.PORT,function(){
+  console.log("Start validate");
+});
